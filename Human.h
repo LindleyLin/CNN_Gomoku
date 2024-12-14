0fe91aafa4ee5cfa435910a4f085a005f98c92a6
Lindley <131772152+LindleyLin@users.noreply.github.com>
2024-12-14 20:47:07 +0800

    Add files via upload

---
+++ Human.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <list>
+#include "Board.h"
+
+struct Human
+{
+	Board board;
+
+	auto get_move(Board board)
+	{
+		short i = 0, j = 0;
+		while (1)
+		{
+			if (board.cur_ply == 1) 
+				printf("O -> ");
+			else 
+				printf("X -> ");
+			printf("Move : ");
+			scanf("%d%d", &i, &j);
+			auto move = make_pair(i, j);
+			auto it = std::find(board.avail.begin(), board.avail.end(), make_pair(i, j));
+			if (it != board.avail.end()) 
+				return move;
+			printf("Invalid Input\n");
+		}
+	}
+};
